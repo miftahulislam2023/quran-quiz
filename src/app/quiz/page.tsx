80d91eac9b87b5eff6154797a85bdb4cb046acee
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2025-04-19 23:18:54 +0600

    working version 2.0 with lots of bugs

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,22 +1,37 @@
 "use client"
 import { useState } from "react";
 import { quranicWords } from "@/lib/data/quranicwords";
+import { Button } from "@/components/ui/button";
 
 export default function Quiz() {
     const [remainingWords, setRemainingWords] = useState(quranicWords);
     const [wordCount, setWordCount] = useState(0);
     const [index, setIndex] = useState(0);
-    const [currentWord, setCurrentWord] = useState(remainingWords[0]);
+    const [currentWord, setCurrentWord] = useState(remainingWords[index]);
     function nextWord() {
-        if (index < remainingWords.length - 1) {
-            setIndex(index + 1);
-            setCurrentWord(remainingWords[index + 1]);
-            setWordCount(wordCount + 1);
+        if (wordCount < quranicWords.length - 1) {
+            // Remove the current word from the remaining words
+            setRemainingWords((prev) => {
+                const newWords = [...prev];
+                newWords.splice(index, 1);
+                return newWords;
+            }
+            );
+            console.log("Remaining words: ", remainingWords);
+            setIndex((
+                Math.floor(Math.random() * (remainingWords.length - 1))
+            ));
+            console.log("Index: ", index);
+            setCurrentWord(remainingWords[index]);
+            console.log("Current word: ", currentWord.arabic);
+            setWordCount((prev) => prev + 1);
+            console.log("Word count: ", wordCount);
         }
         else {
             setIndex(0);
             setCurrentWord(remainingWords[0]);
             setWordCount(0);
+            setRemainingWords(quranicWords);
         }
     }
     return (
@@ -27,12 +42,19 @@ export default function Quiz() {
                 <h2 className="text-xl font-semibold mb-2">{currentWord.arabic}</h2>
                 <p className="text-gray-700 mb-4">{currentWord.bangla}</p>
                 <p className="text-gray-700 mb-4">{currentWord.english}</p>
-                <button
+                <Button
                     onClick={nextWord}
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    variant="default"
                 >
                     Next
-                </button>
+                </Button>
+                {
+                    remainingWords.length > 0 ? (
+                        <p className="text-gray-700 mt-4">Remaining Words: {quranicWords.length - wordCount - 1}</p>
+                    ) : (
+                        <p className="text-red-500 mt-4">No more words left!</p>
+                    )
+                }
             </div>
         </div>
     );
